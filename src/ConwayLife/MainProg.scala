985027f3e6fa9ef1d6a467570cc10c531e6f11b6
Alexey Scripkin <a.scripkin@gmail.com>
2016-12-29 21:14:07 +0300

    main loop

--- src/ConwayLife/MainProg.scala
+++ src/ConwayLife/MainProg.scala
@@ -9,4 +9,13 @@ object MainProg extends App {
 
   val initial = HashSet(CellCoord(0,0), CellCoord(0,1), CellCoord(0,2))
   val w = World(initial)
+
+  def steps(w :World, times :Int, f: World=>Unit) :Unit = {
+    if(times>0) {
+      f(w)
+      steps(w.step(), times-1, f)
+    }
+  }
+
+  steps(w, 10, println)
 }
